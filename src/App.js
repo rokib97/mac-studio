b71dd6d713c6e85bb6a8120431596086fb205ac5
rokib97 <hasanrokib1997@gmail.com>
2022-04-03 20:50:49 +0600

    initial route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Components/About/About";
+import Blogs from "./Components/Blogs/Blogs";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Home from "./Components/Home/Home";
+import NotFound from "./Components/NotFound/NotFound";
+import Reviews from "./Components/Reviews/Reviews";
 function App() {
   return (
     <div className="App">
       <h1>Initial Setup..</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
