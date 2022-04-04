288cf22885975623953e2bb56cf35d925ff9d426
rokib97 <hasanrokib1997@gmail.com>
2022-04-05 01:53:12 +0600

    added 404 error page

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h2>404! Error</h2>
+    <div className="container error-container d-flex justify-content-md-center align-items-center">
+      <div>
+        <h2 className="text-center fw-bold">WE ARE SORRY,PAGE NOT FOUND!</h2>
+        <p className="text-center text-start">
+          THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN REMOVED HAD ITS NAME
+          CHANGED OR IS TEMPORARILY UNAVAILABLE
+        </p>
+        <button
+          onClick={() => navigate("/home")}
+          className="btn btn-lg btn-primary rounded-pill"
+        >
+          BACK TO HOMEPAGE
+        </button>
+      </div>
     </div>
   );
 };
